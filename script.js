712a2385e62dace2fcad31bbdd3a5e9cc0cf4607
Aman Kushwaha <109275885+amankushwaha100@users.noreply.github.com>
2024-09-13 21:53:59 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+// Button
+function createButton(classes) {
+   const button = document.createElement('button');
+   button.className = classes;
+   const icon = createIcon('fa-solid fa-xmark');
+   button.appendChild(icon);
+   return button;
+}
+
+// icon
+function createIcon(classes){
+   const icon = document.createElement('i');
+   icon.className = classes;
+   return icon;
+}
+
+// Event Listeners
+itemForm.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   const newItem = itemInput.value;
+   // Validate Input
+   if(itemInput.value === ''){
+      alert('Please add an item');
+      return;
+   }
+   
+   // Create list item
+   const li = document.createElement('li');
+   li.appendChild(document.createTextNode(newItem));
+   
+   // Create a button
+   const button = createButton('remove-item btn-link text-red');
+   li.appendChild(button);
+   itemList.appendChild(li); 
+   itemInput.value = ''; 
+});
