bb1e6f9658902e244645ce346f1d8bce9ff4a425
Aman Kushwaha <109275885+amankushwaha100@users.noreply.github.com>
2024-09-14 10:05:00 +0530

    Complete Project

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 // Button
 function createButton(classes) {
@@ -27,7 +29,7 @@ itemForm.addEventListener('submit', (e) => {
    if(itemInput.value === ''){
       alert('Please add an item');
       return;
-   }
+    }
    
    // Create list item
    const li = document.createElement('li');
@@ -36,6 +38,52 @@ itemForm.addEventListener('submit', (e) => {
    // Create a button
    const button = createButton('remove-item btn-link text-red');
    li.appendChild(button);
-   itemList.appendChild(li); 
+   itemList.appendChild(li);
+   
+   // Add li to the DOM
+   checkUI();
    itemInput.value = ''; 
+});
+
+itemList.addEventListener('click', (e) =>{
+if(e.target.parentElement.classList.contains('remove-item')){
+   if(confirm('Are you sure?')){
+   e.target.parentElement.parentElement.remove();
+   checkUI();
+ }
+}
+});
+
+clearBtn.addEventListener('click', ()=>{
+   while(itemList.firstChild){
+      itemList.removeChild(itemList.firstChild);
+   } 
+   checkUI();
+});
+
+function checkUI(){
+   const items = itemList.querySelectorAll('li');
+   if(items.length === 0){
+      clearBtn.style.display = 'none';
+      itemFilter.style.display = 'none';
+   }else{
+      clearBtn.style.display = 'block';
+      itemFilter.style.display = 'block';
+   }
+}
+
+checkUI();
+
+itemFilter.addEventListener('input',(e)=>{
+   const items = itemList.querySelectorAll('li');
+   const text = e.target.value.toLowerCase();
+
+   items.forEach(item =>{
+      const itemName = item.firstChild.textContent.toLowerCase();
+      if(itemName.indexOf(text) != -1){
+         item.style.display = 'flex';
+      }else{
+         item.style.display = 'none';
+      }
+   })
 });
